2b98f985cd5cf325ff243b3e402cecddd197ef9f
tomyt-t <eduardotomyt@gmail.com>
2026-09-04 19:49:41 -0300

    Commit 6: Moving method get_charge from Rental to Book.

--- model.py
+++ model.py
@@ -8,26 +8,29 @@ class Book:
         self.title = title
         self.price_code = price_code
 
+    def get_charge(self, days_rented: int) -> float:
+        amount = 0
+
+        if self.price_code == Book.REGULAR:
+            amount += 2
+            if days_rented > 2:
+                amount += (days_rented - 2) * 1.5
+        elif self.price_code == Book.NEW_RELEASE:
+            amount += days_rented * 3
+        elif self.price_code == Book.CHILDREN:
+            amount += 1.5
+            if days_rented > 3:
+                amount += (days_rented - 3) * 1.5
+
+        return amount
+
 class Rental:
     def __init__(self, book: Book, days_rented: int):
         self.book = book
         self.days_rented = days_rented
 
     def get_charge(self) -> float:
-        amount = 0
-
-        if self.book.price_code == Book.REGULAR:
-            amount += 2
-            if self.days_rented > 2:
-                amount += (self.days_rented - 2) * 1.5
-        elif self.book.price_code == Book.NEW_RELEASE:
-            amount += self.days_rented * 3
-        elif self.book.price_code == Book.CHILDREN:
-            amount += 1.5
-            if self.days_rented > 3:
-                amount += (self.days_rented - 3) * 1.5
-
-        return amount
+        return self.book.get_charge(self.days_rented)
 
     def get_frequent_renter_points(self):
         points = 1
@@ -52,8 +55,6 @@ class Client:
         
         for rental in self.rentals:
             amount = rental.get_charge()
-
-            # add frequent renter points
             frequent_renter_points += rental.get_frequent_renter_points()
 
             # show each rental result
